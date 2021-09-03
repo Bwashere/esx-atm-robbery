f2e9b221b9a746a78b4f52ffee93c4f857e7226e
Brackzz <87747715+Bwashere@users.noreply.github.com>
2021-09-02 17:14:31 -0700

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,18 @@
+fx_version 'cerulean'
+games { 'rdr3', 'gta5' }
+
+author 'Brackzz'
+description 'Atm Robbery'
+version '1.0.0'
+
+-- What to run
+client_scripts {
+    'client.lua'
+}
+server_scripts {
+'server.lua'
+}
+
+shared_script {
+'config.lua'
+}
